0b1add2d2618792a86e1b0e704cce281d2ca201b
AriellaRogueN <maitilydevidasi@gmail.com>
2018-05-27 16:56:23 -0400

    version2

--- app.js
+++ app.js
@@ -1,25 +1,41 @@
-order();
-function order()
+let pedido = prompt("Que deseas comer")
+
+order(pedido);
+function order(pedido)
 {   
    let ceviche = 0;
    let tacosAlPastor = 0;
-   let empanadaChilena =0;
+   let empanadaChilena = 0;
    let hamburguesa = 1;
 
-   if (ceviche ==1)
+   if (pedido == "ceviche" & ceviche == 1)
    {
        document.write("Mi orden es ceviche");
    }
-   else if(tacosAlPastor == 1)
+   
+   {
+       pedido = "tacos al pastor"
+   }
+
+   if (pedido == "tacos al pastor" & tacosAlPastor == 1)
    {
        document.write("Mi orden es tacos al pastor");
    }
-   else if (empanadaChilena == 1)
+   else 
+   {
+       pedido = "empanada chilena"
+   }
+
+   if (pedido == "empanada chilena" & empanadaChilena == 1)
    {
        document.write("mi orden es empanada chilena");
    }
-   else if (hamburguesa == 1)
+   else {
+       pedido = "hamburguesa"
+   }
+
+   if (pedido == "hamburguesa" & hamburguesa == 1)
    {
-       document.write("ya que no habia mas me voy por la hamburguesa");
+       document.write("ya que no habia más me voy por la hamburguesa");
    }
 }
